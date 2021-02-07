eb1cf750cc48d00f118fad8b43b32ea56bd92bc5
J. Lewis <inquiries.lewis+dev@gmail.com>
2021-02-07 13:47:04 -0500

    Fix directory shorthand aliases

--- shell/aliases/directories.sh
+++ shell/aliases/directories.sh
@@ -1,22 +1,22 @@
 # Developer
-alias dotfiles/="cdls ${DIR[DOTF]}/"
+alias dotfiles/="cdls \"${DIR[DOTF]}/\""
 
 # Google
-alias gd/="cdls ${DIR[DRIVE]}/"
-alias gd/dev="cdls ${DIR[DRIVE]}/Projects/Dev/"
-alias gd/apollo="cdls ${DIR[DRIVE]}/Projects/Apollo/"
-alias chrome/apps="cdls ${DIR[PWA]}/"
+alias gd/="cdls \"${DIR[DRIVE]}/\""
+alias gd/dev="cdls \"${DIR[DRIVE]}/Projects/Dev/\""
+alias gd/apollo="cdls \"${DIR[DRIVE]}/Projects/Apollo/\""
+alias chrome/apps="cdls \"${DIR[PWA]}/\""
 
 # Documents
-alias docs/="cdls ${DIR[DOCS]}/"
-alias dl/="cdls ${DIR[DL]}/"
-alias dl/docs="cdls ${DIR[DL]}/Documents/"
-alias dl/imgs="cdls ${DIR[DL]}/Images/"
-alias dl/misc="cdls ${DIR[DL]}/Misc/"
+alias docs/="cdls \"${DIR[DOCS]}/\""
+alias dl/="cdls \"${DIR[DL]}/\""
+alias dl/docs="cdls \"${DIR[DL]}/Documents/\""
+alias dl/imgs="cdls \"${DIR[DL]}/Images/\""
+alias dl/misc="cdls \"${DIR[DL]}/Misc/\""
 
 # Leisure
-alias mc/="cdls ${DIR[MC]}/"
-alias mc/mods="cdls ${DIR[MC]}/mods/"
-alias mc/packs="cdls ${DIR[MC]}/resourcepacks/"
-alias mc/saves="cdls ${DIR[MC]}/saves/"
-alias mc/imgs="cdls ${DIR[MC]}/screenshots/"
+alias mc/="cdls \"${DIR[MC]}/\""
+alias mc/mods="cdls \"${DIR[MC]}/mods/\""
+alias mc/packs="cdls \"${DIR[MC]}/resourcepacks/\""
+alias mc/saves="cdls \"${DIR[MC]}/saves/\""
+alias mc/imgs="cdls \"${DIR[MC]}/screenshots/\""
